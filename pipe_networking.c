0c76f5f3181b5c1a25448861b3ac204bb2e8be29
Anand Idris <30372144+aidris823@users.noreply.github.com>
2018-12-19 01:59:28 -0500

    Instead of telling you I finished, I'm gonna recommend you watch this: https://www.youtube.com/watch?v=BduftT_n-Xg

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,7 +11,26 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  return 0;
+  if (mkfifo("Luigi",0644)){
+    printf("Failure to make pipe. =(");
+  }
+  mkfifo("Luigi",0644);
+  
+  int wk_pipe = open("Luigi",O_RDONLY);
+  printf("The server has opened pipe\n");
+  
+  char * client_message = calloc(100,1);
+  read(wk_pipe,client_message,4);
+  printf("Server thinks the client said: %s\n",client_message);
+  
+  int pipe = open(client_message,O_WRONLY);
+  printf("Server opened another pipe for client\n");
+  
+  write(pipe,ACK,sizeof(ACK));
+  
+  *to_client = pipe; //*to_client or to_client?  You say *to_client so I'll take your word.
+  printf("Server has completed its run");
+  return wk_pipe;
 }
 
 
@@ -25,5 +44,21 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  return 0;
+  mkfifo("Kiddo",0644);
+  
+  int wk_pipe = open("Luigi",O_WRONLY);
+  printf("The client has accessed the well known pipe.");
+  
+  write(wk_pipe,"Kiddo",4);
+  
+  int p_pipe = open("Kiddo",O_RDONLY);
+  char * message = calloc(10,1);
+  read(p_pipe,message,10);
+ 
+  *to_server = wk_pipe;
+  printf("The client has shooketh hands.");
+  return p_pipe;
+  //while(1){fork()} is what I feel like doing to my computer.
+  
+
 }
